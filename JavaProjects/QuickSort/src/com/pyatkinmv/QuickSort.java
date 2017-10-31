7bd9b9937d4f079dbb00387b2db09a0facce618a
Maxim <pyatkinmv@gmail.com>
2017-10-31 23:53:42 +0300

    Integer to generics, splitting into two classes

--- JavaProjects/QuickSort/src/com/pyatkinmv/QuickSort.java
+++ JavaProjects/QuickSort/src/com/pyatkinmv/QuickSort.java
@@ -1,32 +1,28 @@
 package com.pyatkinmv;
 
-import java.util.*;
+import java.util.Comparator;
 
-public class QuickSort {
+public class QuickSort <T> {
 
-    static int numOfCompars = 0;
-
-    static void quickSort(Integer[] arr, int first, int last, Comparator c) {
-        if (first < last) {
-            int p = partition(arr, first, last, c);
-            quickSort(arr, first,p - 1, c);
-            quickSort(arr, p, last, c);
+    void sort(T[] arr, int first, int last, Comparator c) {
+            if (first < last) {
+                int p = partition(arr, first, last, c);
+                sort(arr, first, p - 1, c);
+                sort(arr, p, last, c);
+            }
         }
-    }
 
-    static int  partition(Integer[] arr, int first, int  last, Comparator c)  {
-        Integer pivot = getPivot(arr, first, last, c);
+    int partition(T[] arr, int first, int last, Comparator c) {
+        T pivot = getPivot(arr, first, last, c);
         int i = first;
         int j = last;
         while (i <= j) {
-            while (c.compare(arr[i], pivot) < 0) {
+            while (c.compare(arr[i], pivot) < 0)
                 i++;
-            }
-            while (c.compare(arr[j], pivot) > 0) {
+            while (c.compare(arr[j], pivot) > 0)
                 j--;
-            }
             if (i <= j) {
-                Integer temp = arr[i];
+                T temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
                 i++;
@@ -35,71 +31,32 @@ public class QuickSort {
         }
         return i;
     }
-
-    static Integer getPivot(Integer[] arr, int first, int last, Comparator c) {
-        int middle;
-        Integer x = arr[first];
-        Integer y = arr[first + (last-first)/2];
-        Integer z = arr[last];
-        if(c.compare(x, z) < 0) {
-            if (c.compare(y, x) < 0) {
+    T getPivot(T[] arr, int first, int last, Comparator c) {
+        T middle;
+        T x = arr[first];
+        T y = arr[first + (last - first) / 2];
+        T z = arr[last];
+        if (c.compare(x, z) < 0) {
+            if (c.compare(x, y) > 0) {
                 middle = x;
-            }
-            else if(c.compare(y, z) < 0) {
+            } else if (c.compare(y, z) < 0) {
                 middle = y;
-            }
-            else {
+            } else {
                 middle = z;
             }
-        }
-        else {
-            if(c.compare(y, x) > 0)
+        } else {
+            if (c.compare(x, y) < 0)
                 middle = x;
-            else if(c.compare(y, z) > 0) {
+            else if (c.compare(y, z) > 0) {
                 middle = y;
-            }
-            else {
+            } else {
                 middle = z;
             }
         }
         return middle;
     }
 
-    static Integer getMiddlePivot(Integer[] arr, int first, int last) {
-        return arr[first+(last-first)/2];
-    }
-
-    public static void main(String[] args) {
-        Random rand = new Random();
-        Integer[] arr1 = new Integer[100];
-        for(int i=0;i<100;++i)
-            arr1[i] = rand.nextInt(100);
-
-        Integer[] arr2 = Arrays.copyOf(arr1,100);
-
-        quickSort(arr1,0,99, (o1, o2)-> {
-                numOfCompars++;
-                return ((Integer) o1).compareTo((Integer) o2);
-            }
-        );
-
-//        for(Integer value: arr1){
-//            System.out.print(value.intValue()+" ");
-//        }
-//        System.out.println();
-        System.out.println("Number of comparisons in my method: " + numOfCompars);
-
-        numOfCompars = 0;
-
-        Arrays.sort(arr2, (o1, o2)-> {
-            numOfCompars++;
-            return o1.compareTo(o2);
-        });
-
-//        for(Integer value: arr2){
-//            System.out.print(value.intValue()+" ");
-//        }
-//        System.out.println();
-        System.out.println("Number of comparisons in standard method: " + numOfCompars);
-    }
+        T getMiddlePivot(T[] arr, int first, int last) {
+            return arr[first + (last - first) / 2];
+        }
 }
