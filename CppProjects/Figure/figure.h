206ae222dbfd88d9041ffff4b6ab91d51741f027
Maxim <pyatkinmv@gmail.com>
2017-12-06 00:10:18 +0300

    Figure.h was added, figure file was splitted; contains(), MakeFile

---
+++ CppProjects/Figure/figure.h
@@ -0,0 +1,291 @@
+#include <stdio.h>
+ #include <math.h>
+
+ using namespace std;
+ const double  PI =  3.14159;
+ const double EPSILON = 0.0000001;
+
+ class Point {
+ public:
+    double x;
+    double y;
+
+    Point():                         // Default constructor
+        x(0.),
+        y(0.)
+    {}
+
+    Point(const Point& p):        // Copy-constructor
+        x(p.x),
+        y(p.y)
+    {}
+
+    Point(double xx, double yy):
+        x(xx),
+        y(yy)
+    {}
+
+    Point& operator=(const Point& p) {    // Copy-operator
+        x = p.x; y = p.y;
+        return *this;
+    }
+
+    ~Point() {}                              // Destructor
+
+    Point operator+(const Point& p) const {
+        return Point(x+p.x, y+p.y);
+    }
+
+    Point operator-(const Point& p) const {
+        return Point(x-p.x, y-p.y);
+    }
+
+    Point& operator+=(const Point& p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point& operator-=(const Point& p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point operator*(double c) const {
+        return Point(x*c, y*c);
+    }
+
+    Point& operator*=(double c) {
+        x *= c;
+        y *= c;
+        return *this;
+    }
+
+    double distance (const Point& b) const {
+        return sqrt(pow(x - b.x, 2) + pow(y - b.y, 2));
+    }
+
+    // Comparings
+    bool operator==(const Point& p) const {
+        //... return (x == p.x && y == p.y);
+        return (
+            fabs(x - p.x) <= EPSILON &&
+            fabs(y - p.y) <= EPSILON
+        );
+    }
+    bool operator!=(const Point& p) const { return !operator==(p); }
+    bool operator>=(const Point& p) const {
+        //... return (x > p.x || (x == p.x && y >= p.y));
+        return (x > p.x || (x >= p.x && y >= p.y));
+    }
+    bool operator>(const Point& p) const {
+        //... return (x > p.x || (x == p.x && y > p.y));
+        return (x > p.x || (x >= p.x && y > p.y));
+    }
+    bool operator<(const Point& p) const { return !operator>=(p); }
+    bool operator<=(const Point& p) const { return !operator>(p); }
+    void showDescription() {
+       printf("Point");
+    }
+};
+
+class Figure {
+public:
+    //конструктор класса фигуры
+    Figure()
+    {}
+
+    virtual double getSquare() {
+        return 0;
+    }
+    virtual void showDescription() {
+        printf("Abstract Figure ");
+    }
+    virtual bool contains(const Point& p2) const {
+        return false;
+    }
+};
+
+class Circle: public Figure {
+    Point centre;
+    double radius;
+public:
+    Circle():
+        centre(Point(0., 0.)),
+        radius(0.)
+    {}
+
+    Circle(const Point& _centre, double _radius):
+        centre(centre),
+        radius(_radius)
+    {}
+
+
+    Circle(const Circle& c):
+        centre(c.centre),
+        radius(c.radius)
+    {}
+
+    ~Circle() {}
+
+    Circle& operator=(const Circle& c) {
+        centre = c.centre;
+        radius = c.radius;
+        return *this;
+    }
+
+    bool operator==(const Circle& c) const {
+        return (
+            fabs(centre.distance(c.centre)) <= EPSILON &&
+            fabs(radius - c.radius) <= EPSILON );
+    }
+    bool operator!=(const Circle& c) const { return !operator==(c); }
+
+    Point getCentre() const { return centre; }
+    double getRadius() const { return radius; }
+
+    void setCentre(Point _centre) { centre = _centre; }
+    void setRadius(double _radius) { radius = _radius; }
+
+    bool contains(const Point& p2) const {
+        return centre.distance(p2) <= radius;
+    }
+
+    double getSquare() {
+       return PI * pow(radius, 2);
+    }
+
+    void showDescription() {
+        printf("Circle");
+    }
+};
+
+class Rectangle: public Figure {
+    Point p;
+    double width;   // width
+    double height;   // height
+public:
+    Rectangle():
+        p(Point(0.,0.)),
+        width(0.),
+        height(0.)
+    {}
+
+    Rectangle(const Point& p2, double _width, double _height):
+        p(p2),
+        width(_width),
+        height(_height)
+    {}
+
+    Rectangle(const Rectangle& r):
+        p(r.p),
+        width(r.width),
+        height(r.height)
+    {}
+
+     ~Rectangle() {}
+
+    Rectangle& operator=(const Rectangle& r) {
+        p = r.p;
+        width = r.width;
+        height = r.height;
+        return *this;
+    }
+
+    bool operator==(const Rectangle& rect) const {
+        return (
+            fabs(p.distance(rect.getLeftBottom())) <= EPSILON &&
+            fabs(width - rect.getWidth()) <= EPSILON &&
+            fabs(height - rect.getHeight()) <= EPSILON
+         );
+    }
+    bool operator!=(const Rectangle& rect) const { return !operator==(rect); }
+
+
+    Point getLeftBottom() const { return p; }
+    double getWidth() const { return width; }
+    double getHeight() const { return height; }
+
+    void setLeftBottom(Point p2) { p = p2; }
+    void setWidth(double _width) { width = _width; }
+    void setHeight(double _height) { height = _height; }
+
+
+    double getSquare() {
+       return width * height;
+    }
+
+    bool contains(const Point& p2) const {
+        return (
+            p.x <= p2.x && p2.x < p.x + width &&
+            p.y <= p2.y && p2.y < p.y + height
+        );
+    }
+
+    void showDescription() {
+        printf("Rectangle");
+    }
+ };
+
+class Triangle: public Figure {
+    Point a;
+    Point b;
+    Point c;
+public:
+    Triangle():
+        a(Point(0.,0.)),
+        b(Point(0.,0.)),
+        c(Point(0.,0.))
+    {}
+
+    Triangle(const Point& _a, const Point& _b, const Point& _c):
+        a(_a),
+        b(_b),
+        c(_c)
+    {}
+
+    Triangle(const Triangle& t):
+        a(t.a),
+        b(t.b),
+        c(t.c)
+    {}
+
+     ~Triangle() {}
+
+    Triangle& operator=(const Triangle& t) {
+        a = t.a;
+        b = t.b;
+        c = t.c;
+        return *this;
+    }
+
+    bool operator==(const Triangle& t) const {
+        return (
+            fabs(a.distance(t.getA())) <= EPSILON &&
+            fabs(b.distance(t.getB())) <= EPSILON &&
+            fabs(c.distance(t.getC())) <= EPSILON
+         );
+    }
+    bool operator!=(const Triangle& t) const { return !operator==(t); }
+
+
+    Point getA() const { return a; }
+    Point getB() const { return b; }
+    Point getC() const { return c; }
+
+    void setA(Point _a) { a = _a; }
+    void setB(Point _b) { b = _b; }
+    void setC(Point _c) { c = _c; }
+
+    double getSquare() {
+        double p = (a.distance(b) + b.distance(c) + a.distance(c))/2;
+        return sqrt(p*(p - a.distance(b))*(p - b.distance(c))*(p - a.distance(c)));
+    }
+
+    bool contains(const Point& p) const;
+
+    void showDescription() {
+        printf("Triangle");
+    }
+};
---
+++ CppProjects/Figure/figureTst.cpp
@@ -0,0 +1,69 @@
+#include "figure.h"
+#include <stdio.h>
+
+int main() {
+    double radius, width, height;
+    double x, y;
+    while(true) {
+
+        printf("Coordinates of central point of circle: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+            break;
+        Point centre = Point(x, y);
+
+        printf("Radius of circle: \n");
+        if(scanf("%lf",  &radius) < 1)
+            break;
+
+        Circle circle = Circle(centre, radius);
+        printf("Square of ");
+        circle.showDescription();
+        printf(" is %lf \n",circle.getSquare());
+
+
+        printf("Coordinates of left bottom point of rectangle: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+            break;
+        Point leftBot = Point(x, y);
+
+        printf("Enter width of rectangle: \n");
+        if(scanf("%lf",  &width) < 1)
+            break;
+
+        printf("Enter height of rectangle: \n");
+        if(scanf("%lf",  &height) < 1)
+            break;
+        Rectangle rect = Rectangle(leftBot, width, height);
+       printf("Square of ");
+        rect.showDescription();
+        printf(" is %lf \n ",rect.getSquare());
+
+
+        printf("Coordinates of a point of triangle: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+            break;
+        Point a = Point(x, y);
+        printf("Coordinates of b point of triangle: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+          break;
+        Point b = Point(x, y);
+        printf("Coordinates of c point of triangle: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+          break;
+        Point c = Point(x, y);
+        Triangle tr = Triangle(a, b, c);
+        printf("Square of ");
+        tr.showDescription();
+        printf(" is %lf \n ",tr.getSquare());
+
+
+        printf("Coordinates of some point: \n");
+        if(scanf("%lf %lf", &x, &y) < 2)
+            break;
+        Point point = Point(x,y);
+        printf("The point %s in the circle\n", circle.contains(point) ? "is" : "is not");
+        printf("The point %s in the rectangle \n", rect.contains(point) ? "is" : "is not");
+        printf("The point %s in the triangle \n", tr.contains(point) ? "is" : "is not");
+    }
+    return 0;
+}
