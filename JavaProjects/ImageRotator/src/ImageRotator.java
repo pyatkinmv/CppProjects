456251f97ab12c99efef82edad9c05e3a90a1123
pyatkinmv <pyatkinmv@gmail.com>
2018-12-15 23:54:10 +0300

    Rotate without extending/cutting

--- JavaProjects/ImageRotator/src/ImageRotator.java
+++ JavaProjects/ImageRotator/src/ImageRotator.java
@@ -1,47 +1,13 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class ImageRotator {
 
-    private int[][] extendMtrx(int[][] matrix) {
-        int height = matrix.length;
-        int width = matrix[0].length;
-        int maxDim = Math.max(height, width);
-        int[][] extended = new int[maxDim][maxDim];
-
-        for (int i = 0; i < height; ++i)
-            for (int j = 0; j < width; ++j)
-                extended[i][j] = matrix[i][j];
-
-        return extended;
-    }
-
-    private int[][] cutMatrix(int[][] matrix, int height, int width) {
-        int[][] cutted = new int[height][width];
-
-        if (height > width) {
-            for (int i = 0; i < height; ++i)
-                for (int j = 0; j < width; ++j)
-                    cutted[i][j] = matrix[i][j + matrix[0].length - width];
-        }
-
-        if (height < width) {
-            for (int i = 0; i < height; ++i)
-                for (int j = 0; j < width; ++j)
-                    cutted[i][j] = matrix[i][j];
-        }
-
-        return cutted;
-    }
-
     private int[][] intArrayFromImage(BufferedImage bufferedImage) {
-        int width = bufferedImage.getWidth(null);
-        int height = bufferedImage.getHeight(null);
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
         int[][] arrImage = new int[width][height];
 
         for (int i = 0; i < width; i++) {
@@ -49,7 +15,6 @@ public class ImageRotator {
                 arrImage[i][j] = bufferedImage.getRGB(i, j);
             }
         }
-
         return arrImage;
     }
 
@@ -65,39 +30,72 @@ public class ImageRotator {
         return image;
     }
 
-    private void rotateSquareMatrix(int[][] matrix) {
+    private void rotateSquareMatrix(int[][] matrix, boolean clockwise) {
         int N = matrix.length - 1;
         int k1 = N / 2;
         int k2 = (N % 2 == 0) ? N / 2 - 1 : N / 2;
 
-        for (int i = 0; i <= k1; ++i) {
-            for (int j = 0; j <= k2; ++j) {
-                int topLeft = matrix[i][j];
-                int topRight = matrix[j][N - i];
-                int botRigth = matrix[N - i][N - j];
-                int botLeft = matrix[N - j][i];
+        if (clockwise) {
+            for (int i = 0; i <= k1; ++i) {
+                for (int j = 0; j <= k2; ++j) {
+                    int topLeft = matrix[i][j];
+                    int topRight = matrix[j][N - i];
+                    int botRigth = matrix[N - i][N - j];
+                    int botLeft = matrix[N - j][i];
+                    
+                    matrix[j][N - i] = topLeft;
+                    matrix[N - i][N - j] = topRight;
+                    matrix[N - j][i] = botRigth;
+                    matrix[i][j] = botLeft;
+                }
+            }
+        } else {
+            for (int i = 0; i <= k1; ++i) {
+                for (int j = 0; j <= k2; ++j) {
+                    int topLeft = matrix[i][j];
+                    int topRight = matrix[j][N - i];
+                    int botRigth = matrix[N - i][N - j];
+                    int botLeft = matrix[N - j][i];
 
-                matrix[j][N - i] = topLeft;
-                matrix[N - i][N - j] = topRight;
-                matrix[N - j][i] = botRigth;
-                matrix[i][j] = botLeft;
+                    matrix[N - j][i] = topLeft;
+                    matrix[i][j] = topRight;
+                    matrix[j][N - i] = botRigth;
+                    matrix[N - i][N - j]= botLeft;
+                }
             }
         }
     }
 
-    private int[][] rotateMatrix(int[][] matrix) {
+    private int[][] rotateMatrix(int[][] matrix, boolean clockwise) {
         int height = matrix.length;
         int width = matrix[0].length;
-        int[][] squareMatrix = (height == width) ? matrix : extendMtrx(matrix);
+        int[][] rotated = new int[width][height];
 
-        rotateSquareMatrix(squareMatrix);
-
-        return (height == width) ? squareMatrix : cutMatrix(squareMatrix, width, height);
+        if (clockwise) {
+            for (int i = 0; i < height; ++i) {
+                for (int j = 0; j < width; ++j) {
+                    rotated[width - j - 1][i] = matrix[i][j];
+                }
+            }
+        } else {
+            for (int i = 0; i < height; ++i) {
+                for (int j = 0; j < width; ++j) {
+                    rotated[j][height - i - 1] = matrix[i][j];
+                }
+            }
+        }
+        return rotated;
     }
 
-    public BufferedImage rotate(BufferedImage image) {
+    public BufferedImage rotate(BufferedImage image, boolean clockwise) {
         int[][] arrImage = intArrayFromImage(image);
-        int[][] rotatedArray = rotateMatrix(arrImage);
+
+        if (arrImage.length == arrImage[0].length) {
+            rotateSquareMatrix(arrImage, clockwise);
+            return imageFromIntArray(arrImage);
+        }
+
+        int[][] rotatedArray = rotateMatrix(arrImage, clockwise);
 
         return imageFromIntArray(rotatedArray);
     }
--- JavaProjects/ImageRotator/src/Main.java
+++ JavaProjects/ImageRotator/src/Main.java
@@ -5,12 +5,34 @@ import java.io.IOException;
 
 public class Main {
 
+    public static void print(int[][] matrix) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                System.out.print(String.format("%3d ", matrix[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] createMatrix(int height, int width) {
+        int[][] matrix = new int[height][width];
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                matrix[i][j] = i * height + j;
+            }
+        }
+        return matrix;
+    }
+
     public static void main(String[] args) {
-        File fileImage = new File("result_image.jpg");
+        File fileImage = new File("image.jpg");
         try {
             BufferedImage image = ImageIO.read(fileImage);
             ImageRotator rotator = new ImageRotator();
-            BufferedImage rotatedImage = rotator.rotate(image);
+            BufferedImage rotatedImage = rotator.rotate(image, true);
             File output = new File("result_image.jpg");
             ImageIO.write(rotatedImage, "jpg", output);
         } catch (IOException e) {
